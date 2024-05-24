0ba9210a99fa52f304cceb8a1e290bf669e5bb0d
Anil <anilyadavjnt@gmail.com>
2024-05-24 13:52:57 +0530

    Initial Commit

---
+++ Xylophone/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Anil Kumar Yadav Yu on 24/05/2024.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func keyPressed(_ sender: UIButton) {
+        
+        
+        playSound(soundName: sender.currentTitle!)
+        
+    }
+    
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
+}
+
